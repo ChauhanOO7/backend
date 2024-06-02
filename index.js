002441063dc56da1f82a72ab06e4b1ec0295bb8e
ChauhanOO7 <saurabhc7388@gmail.com>
2024-06-02 16:13:07 +0530

    third commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const scrapeTwitterTrends = require('./selenium-script');
 const cors=require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.use(express.static('../frontend'));
 app.use(cors());
